504482d0de6cf9cf6d939a2cd8f04fd8fb4a6ff8
Fahad Jawad <fahadjawad596@gmail.com>
2025-01-19 00:07:17 +0500

    added max-height for the project card

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -5,13 +5,14 @@ import Image from 'next/image';
 
 export default function ProjectCard() {
   return (
-    <div className='projectCard my-2 lg:m-2 relative group'>
+    <div className='projectCard my-2 lg:m-2 relative group max-h-[400px] overflow-hidden'>
       <ReactCompareImage
-        leftImage='/images/modern-cabinets.png'
-        rightImage='/images/modern-cabinets2.png'
-        leftImageCss={{borderRadius:'8px'}}
-        rightImageCss={{borderRadius:'8px'}}
+        leftImage='/images/before.jpg'
+        rightImage='/images/after.jpg'
+        leftImageCss={{borderRadius:'8px',maxHeight:'400px'}}
+        rightImageCss={{borderRadius:'8px',maxHeight:'400px'}}
         handleSize={25}
+        aspectRatio='wider'
       />
 
   
